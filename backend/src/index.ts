7204704a9f8cf2513cce8163407d70c82d5489e6
shreedhar01 <shreedharkalamagar@gmail.com>
2026-03-26 19:36:36 +0545

    feat: express setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello wor")
+import express from "express"
+import { PORT } from "./config/env.js"
+
+const app = express()
+
+app.get("/",(_,res)=>{
+    res.json({message:"Server is up and running"})
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is running in port :: ",PORT)
+})
