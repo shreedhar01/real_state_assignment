82c408c0cef9d53fb1a6c36b0e7d1c138380d4a2
shreedhar01 <shreedharkalamagar@gmail.com>
2026-03-27 14:48:06 +0545

    feat: shadcn added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
-    <div className='flex items-center justify-center w-full min-h-screen'>
+    <div className='flex flex-col items-center justify-center w-full min-h-screen'>
       <p className="bg-black text-white p-4 rounded-xl">Hello world</p>
+      <Button>Click me</Button>
     </div>
   )
 }
