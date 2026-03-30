fe2906fbb488b64a26e8ff36ec99cfbb5b43dbff
shreedhar01 <shreedharkalamagar@gmail.com>
2026-03-30 12:58:02 +0545

    feat: dockerfile for backend

--- backend/src/api_v1/controller/property.controller.ts
+++ backend/src/api_v1/controller/property.controller.ts
@@ -10,8 +10,8 @@ export const getAllPropertyController = asyncHandler(async (req: Request, res: R
     const pageNumber = Math.max(1, Number(page) || 1)
     const limitNumber = Math.min(50, Number(limit) || 10)
 
-    console.log("pageNumber", pageNumber)
-    console.log("limitNumber", limitNumber)
+    // console.log("pageNumber", pageNumber)
+    // console.log("limitNumber", limitNumber)
 
     const allProperty = await getAllPropertyService(pageNumber, limitNumber, req.user!.id)
     return res.status(200).json(
